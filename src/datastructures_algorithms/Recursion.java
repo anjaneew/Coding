f52283f88abf04bc60e8b5b60c02cc66f9b329e9
Anjanee Wijewardana <anjaneew@gmail.com>
2025-06-23 09:09:01 +0300

    Recursion

--- src/datastructures_algorithms/Recursion.java
+++ src/datastructures_algorithms/Recursion.java
@@ -9,9 +9,6 @@ public class Recursion {
         System.out.println(power(2, 8));
     }
 
-
-
-
     //iterative approach - repetition of an internal process
     private static void walkItiritively(int steps) {
 
@@ -22,10 +19,8 @@ public class Recursion {
 
     //recursion = repetition of a procedure
     private static void walkRecursively(int steps) {
-
         //base case - what we do when we like to stop
         if(steps < 1) return;
-
 
         System.out.println("walk recursively");
         walkRecursively(steps - 1);//recursive case -
@@ -33,18 +28,12 @@ public class Recursion {
     }
 
     //LIFO
-
-
     private static int factorial(int num) {
-
         if(num < 1) return 1; //base case
         return num * factorial(num - 1);//recursive case
-
     }
 
-
     private static int power(int base, int exponent) {
-
       if(exponent < 1) return 1; //base case
       return base * power(base, exponent -1);//recursive case
     }

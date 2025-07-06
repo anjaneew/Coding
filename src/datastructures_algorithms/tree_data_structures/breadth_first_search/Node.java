5401c57ccdddf115266f93402c1d2c2f3ad15b46
Anjanee Wijewardana <anjaneew@gmail.com>
2025-07-06 07:20:37 +0300

    Depth First Search

---
+++ src/datastructures_algorithms/tree_data_structures/breadth_first_search/Node.java
@@ -0,0 +1,11 @@
+package datastructures_algorithms.tree_data_structures.breadth_first_search;
+
+public class Node {
+
+    char data;
+//    boolean visited;
+
+    Node(char data){
+        this.data =  data;
+    }
+}
---
+++ src/datastructures_algorithms/tree_data_structures/depth_first_search/Node.java
@@ -0,0 +1,11 @@
+package datastructures_algorithms.tree_data_structures.depth_first_search;
+
+public class Node {
+
+    char data;
+//    boolean visited;
+
+    Node(char data){
+        this.data =  data;
+    }
+}

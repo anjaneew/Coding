b57916cbfb595ee4010105285dc4996a7ceacac9
Anjanee Wijewardana <anjaneew@gmail.com>
2025-07-07 07:28:26 +0300

    Binary Search Tree

---
+++ src/datastructures_algorithms/treedatastructures/binarytree/Main.java
@@ -0,0 +1,6 @@
+package datastructures_algorithms.treedatastructures.binarytree;
+
+public class Main {
+    //create Binary Search Tree object
+    BinarySearchTree tree = new BinarySearchTree();
+}
---
+++ src/datastructures_algorithms/treedatastructures/binarytree/Node.java
@@ -0,0 +1,13 @@
+package datastructures_algorithms.treedatastructures.binarytree;
+
+public class Node {
+    int data;
+    Node left;
+    Node right;
+
+    //constructor
+    public Node(int data){
+        this.data = data;
+    }
+
+}
